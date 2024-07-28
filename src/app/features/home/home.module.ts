4840f5eb8d37cfebb63006b5ce35de6cb21d8494
Tomás Mendes <tomashm16@gmail.com>
2024-07-28 19:49:45 +0200

    refactor: some small refactoring

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
@@ -23,6 +23,7 @@ import { MatButton } from '@angular/material/button';
     FormsModule,
     MatButton,
     SharedModule,
+    NgOptimizedImage,
   ],
 })
 export class HomeModule {}
